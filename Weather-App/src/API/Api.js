f36fb722a5f8852ef20659b796284ea8a965901d
eozkanch <chofficelch@gmail.com>
2023-06-17 23:32:48 +0200

    last2

--- Weather-App/src/API/Api.js
+++ Weather-App/src/API/Api.js
@@ -1,4 +1,3 @@
-
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import CurrentDay from '../components/CurrentDay';
@@ -8,10 +7,7 @@ import { BgImages } from '../components/BgImages';
 import { Spinner } from 'react-bootstrap';
 
 const Api = () => {
-
   const apiKey = process.env.REACT_APP_GHOST_API_KEY;
-
-
 
   const [data, setData] = useState({});
   const [search, setSearch] = useState('');
@@ -28,7 +24,9 @@ const Api = () => {
     async function getApi() {
       setLoading(true);
       try {
-        const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${search}&days=3&aqi=yes&alerts=no`);
+        const response = await axios.get(
+          `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${search}&days=3&aqi=yes&alerts=no`
+        );
         setData(response.data);
         console.log(response.data);
         const city = response.data?.location?.tz_id.split('/')[1];
@@ -103,11 +101,11 @@ const Api = () => {
         ? `url(${BgImages.sunny})`
         : condition === 'Clear'
         ? `url(${BgImages.clear})`
-        : (condition === 'Cloudy' ||
+        : condition === 'Cloudy' ||
           condition === 'Partly cloudy' ||
-          condition === 'Heavy cloudy')
+          condition === 'Heavy cloudy'
         ? `url(${BgImages.cloudy})`
-        : (condition === 'Light rain shower' ||
+        : condition === 'Light rain shower' ||
           condition === 'Patchy rain possible' ||
           condition === 'Moderate rain' ||
           condition === 'Patchy light drizzle' ||
@@ -116,20 +114,17 @@ const Api = () => {
           condition === 'Thundery outbreaks possible' ||
           condition === 'Moderate or heavy rain with thunder' ||
           condition === 'Light rain' ||
-          condition === 'Patchy light rain with thunder')
+          condition === 'Patchy light rain with thunder'
         ? `url(${BgImages.rain})`
-        : (condition === 'Storm' ||
-          condition === 'Heavy rain at times')
+        : condition === 'Storm' || condition === 'Heavy rain at times'
         ? `url(${BgImages.storm})`
-        : (condition === 'Snowstorm' ||
+        : condition === 'Snowstorm' ||
           condition === 'Irregular light snow' ||
           condition === 'Light snow' ||
           condition === 'Medium heavy snow' ||
-          condition === 'Thick with snow')
+          condition === 'Thick with snow'
         ? `url(${BgImages.snow})`
-        : (condition === 'Mist' ||
-          condition === 'Overcast' ||
-          condition === 'Fog')
+        : condition === 'Mist' || condition === 'Overcast' || condition === 'Fog'
         ? `url(${BgImages.overcast})`
         : `url(${BgImages.general})`
       : `url(${BgImages.general})`,
@@ -137,44 +132,40 @@ const Api = () => {
 
   return (
     <div className="api" style={{ background: conditionStyle.background }}>
-     
-          <form onSubmit={onSubmit}>
-            <div className="search-cont">
-              {showCountryFlag && <img className="flag-animation" src={flagUrl} alt="Country Flag" />}
-              <input
-                onChange={(e) => setValue(e.target.value)}
-                value={value}
-                type="text"
-                className="search-input"
-                placeholder="Please enter a city name..."
-              />
-              <button type="submit" className="search-btn">
-                {' '}
-                <BsSearch size={30} />{' '}
-              </button>
-           
-              {showCountryFlag && (
-                  <div className={`icon-wrapper ${isEvening ? 'evening' : ''}`}>
-                    {isEvening ? (
-                      <BsMoonStars color="white" size={60} />
-                    ) : (
-                      <FaSun color="yellow" size={60} />
-                    )}
-                  </div>
-                )}
+      <form onSubmit={onSubmit}>
+        <div className="search-cont">
+          {showCountryFlag && <img className="flag-animation" src={flagUrl} alt="Country Flag" />}
+          <input
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+            type="text"
+            className="search-input"
+            placeholder="Please enter a city name..."
+          />
+          <button type="submit" className="search-btn">
+            {' '}
+            <BsSearch size={30} />{' '}
+          </button>
 
+          {showCountryFlag && (
+            <div className={`icon-wrapper ${isEvening ? 'evening' : ''}`}>
+              {isEvening ? (
+                <BsMoonStars color="white" size={60} />
+              ) : (
+                <FaSun color="yellow" size={60} />
+              )}
             </div>
-          </form>
-
-          {loading ? (
-            <Spinner animation="grow" />
-          ) : (
-            <>
-              {Object.entries(data).length !== 0 && <CurrentDay data={data} cityTime={cityTime} />}
-            </>
           )}
-       
-      
+        </div>
+      </form>
+
+      {loading ? (
+        <Spinner animation="grow" />
+      ) : (
+        <>
+          {Object.entries(data).length !== 0 && <CurrentDay data={data} cityTime={cityTime} />}
+        </>
+      )}
     </div>
   );
 };
